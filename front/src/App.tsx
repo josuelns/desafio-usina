29d01ca632310cc1dbb0e62b02f527cb8ddc3f83
Josue <josuenavarro2000@hotmail.com>
2024-11-02 18:47:57 -0300

    feat: title page
    Adicionado o titulo da pagina

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -2,6 +2,7 @@ import logo from './assets/images/logo.png';
 import fundo from './assets/images/fundo.png';
 
 import './global.css';
+import { Filter, Search } from 'lucide-react';
 
 const App = () => {
   return (
@@ -12,64 +13,64 @@ const App = () => {
         backgroundSize: 'cover',
         backgroundPosition: 'center',
         backgroundRepeat: 'no-repeat',
-        minHeight: '65vh',
+        minHeight: '70vh',
       }}
     >
+      <div className="absolute inset-0 bg-[#00000080]">
+        <header className="relative flex justify-between bg-transparent shadow-md px-6 py-4">
+          <div className='mx-4'>
+            <img src={logo} alt="RecomendaFlix Logo" className="h-40 min-w-40 w-auto" />
+          </div>
 
-      <div className="absolute inset-0 bg-black opacity-50"></div>
+          <nav className='flex items-start pt-12 -ml-16'>
+            <ul className='flex gap-4 text-white py-2'>
+              <li className='hover:text-red-600 cursor-pointer uppercase'>Início</li>
+              <li className='relative group'>
+                <span className='hover:text-red-600 cursor-pointer uppercase'>Gêneros</span>
+                <ul className='absolute left-0 hidden group-hover:flex flex-col gap-2 bg-gray-800 p-2 max-h-40 w-52 overflow-y-auto'>
+                  {["Ação", "Aventura", "Animação", "Biografia", "Comédia", "Comédia Romântica", "Documentário", "Drama", "Fantasia", "Ficção Científica", "Mistério", "Musical", "Musical Dramático", "Romance", "Slasher", "Survival", "Terror", "Thriller", "Western", "Cyberpunk", "Noir", "Filme de Guerra", "Filme de Super-Herói", "Mockumentary", "Feel-good"].map(genero => (
+                    <li key={genero} className='hover:text-red-600 cursor-pointer uppercase'>{genero}</li>
+                  ))}
+                </ul>
+              </li>
+              <li className='hover:text-red-600 cursor-pointer uppercase'>Em Alta</li>
+              <li className='hover:text-red-600 cursor-pointer uppercase'>Novidades</li>
+            </ul>
+          </nav>
 
-      <div className="relative flex flex-1 justify-between bg-transparent shadow-md px-6">
-        <div>
-          <img src={logo} alt="Logo" className="h-40 min-w-20 w-auto" />
+          <div className='flex items-start pt-12'>
+            <button className="border-2 border-white text-white bg-transparent py-2 px-4 rounded transition duration-300 hover:bg-red-600 hover:border-red-600 shadow-md">
+              LOGIN
+            </button>
+          </div>
+        </header>
+
+        <div className='-mt-8'>
+          <div className="text-center text-white">
+            <h1 className="text-base tracking-wide uppercase text-gray-300 mb-4">RECOMENDAFLIX</h1>
+            <h2 className="text-3xl md:text-5xl font-bold leading-tight">
+              SEU <span className="text-red-600">GUIA</span> PARA <br className="hidden md:block" /> O MELHOR DO CINEMA
+            </h2>
+            <div className='flex justify-center items-center mt-6'>
+
+              <div className="flex w-[70%]">
+                <input
+                  type="text"
+                  placeholder="Pesquisar..."
+                  className="px-4 py-2 border rounded-l focus:outline-none w-full"
+                />
+                <div className='flex mx-2 gap-1'>
+                  <button className="bg-red-600 text-white px-4 hover:bg-red-700 transition duration-300 shadow-md rounded-sm">
+                    <Search className="inline w-5 h-5" />
+                  </button>
+                  <button className="bg-gray-800 text-white px-4 ml-2 hover:bg-gray-700 transition duration-300 shadow-md rounded-sm">
+                    <Filter className="inline w-5 h-5" />
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-
-        <div className='flex items-start pt-12'>
-          <ul className='flex gap-4 text-white py-2'>
-            <li className='hover:text-red-600 cursor-pointer uppercase'>Início</li>
-            <li className='relative group'>
-              <span className='hover:text-red-600 cursor-pointer uppercase'>Gêneros</span>
-              <ul className='absolute left-0 hidden group-hover:flex flex-col gap-2 bg-gray-800 p-2 max-h-40 w-52 overflow-y-auto'>
-                <li className='hover:text-red-600 cursor-pointer uppercase'>Ação</li>
-                <li className='hover:text-red-600 cursor-pointer uppercase'>Aventura</li>
-                <li className='hover:text-red-600 cursor-pointer uppercase'>Animação</li>
-                <li className='hover:text-red-600 cursor-pointer uppercase'>Biografia</li>
-                <li className='hover:text-red-600 cursor-pointer uppercase'>Comédia</li>
-                <li className='hover:text-red-600 cursor-pointer uppercase'>Comédia Romântica</li>
-                <li className='hover:text-red-600 cursor-pointer uppercase'>Documentário</li>
-                <li className='hover:text-red-600 cursor-pointer uppercase'>Drama</li>
-                <li className='hover:text-red-600 cursor-pointer uppercase'>Fantasia</li>
-                <li className='hover:text-red-600 cursor-pointer uppercase'>Ficção Científica</li>
-                <li className='hover:text-red-600 cursor-pointer uppercase'>Mistério</li>
-                <li className='hover:text-red-600 cursor-pointer uppercase'>Musical</li>
-                <li className='hover:text-red-600 cursor-pointer uppercase'>Musical Dramático</li>
-                <li className='hover:text-red-600 cursor-pointer uppercase'>Romance</li>
-                <li className='hover:text-red-600 cursor-pointer uppercase'>Slasher</li>
-                <li className='hover:text-red-600 cursor-pointer uppercase'>Survival</li>
-                <li className='hover:text-red-600 cursor-pointer uppercase'>Terror</li>
-                <li className='hover:text-red-600 cursor-pointer uppercase'>Thriller</li>
-                <li className='hover:text-red-600 cursor-pointer uppercase'>Western</li>
-                <li className='hover:text-red-600 cursor-pointer uppercase'>Cyberpunk</li>
-                <li className='hover:text-red-600 cursor-pointer uppercase'>Noir</li>
-                <li className='hover:text-red-600 cursor-pointer uppercase'>Filme de Guerra</li>
-                <li className='hover:text-red-600 cursor-pointer uppercase'>Filme de Super-Herói</li>
-                <li className='hover:text-red-600 cursor-pointer uppercase'>Mockumentary</li>
-                <li className='hover:text-red-600 cursor-pointer uppercase'>Feel-good</li>
-              </ul>
-            </li>
-            <li className='hover:text-red-600 cursor-pointer uppercase'>Em Alta</li>
-            <li className='hover:text-red-600 cursor-pointer uppercase'>Novidades</li>
-          </ul>
-        </div>
-
-        <div className='flex items-start pt-12'>
-          <button className="border-2 border-white text-white bg-transparent py-2 px-4  rounded transition duration-300 hover:bg-red-600 hover:border-red-600 hover:text-white shadow-md ">
-            LOGIN
-          </button>
-        </div>
-      </div>
-
-      <div>
-
       </div>
     </div>
   );
