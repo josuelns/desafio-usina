a07f3c5593be562a94a654f1ac62fee7978c6d9f
Josue <josuenavarro2000@hotmail.com>
2024-11-06 19:31:48 -0300

    feat config db

---
+++ backend/src/config.ts
@@ -0,0 +1,11 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+export const dbConfig = {
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: Number(process.env.DB_PORT),
+};
